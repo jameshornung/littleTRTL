98c18733aee23d1f30bc92505d8b4ca9c5e30006
jameshornung <hornung.james@gmail.com>
2017-07-26 14:15:43 -0500

    removed commented out code

--- src/components/AddCandidates.js
+++ src/components/AddCandidates.js
@@ -14,8 +14,6 @@ class AddCandidates extends Component {
       status: '',
     };
 
-    // console.log(this.state);
-
     this.addFirstName = this.addFirstName.bind(this);
     this.addLastName = this.addLastName.bind(this);
     this.addUniversity = this.addUniversity.bind(this);
@@ -90,14 +88,7 @@ class AddCandidates extends Component {
 
 export default AddCandidates;
 
-// document.getElementById("addProspect").addEventListener("click", function(){
 
-//   var data = "firstName=" + document.getElementById("firstName").value + "&lastName=" + document.getElementById("lastName").value + "&university=" + document.getElementById("university").value + "&status=" + document.getElementById("status").value;
-
-//   request.open('POST', '/submit', true);
-//   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-//   request.send(data);
-//   })
 
 
   
